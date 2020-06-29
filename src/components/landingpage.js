094eb006d4449dccb1dcb56cf40b594e4dbaed78
Francisco Gallegos <fgallegosgo@gmail.com>
2020-06-29 17:39:50 -0600

    add some adjustments

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Landing page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="https://www.sergiserramenti.it/images/max.png"
+                            alt="profile"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Software Engineer</h1>
+                            
+                            <hr />
+                            
+                            <p> HTML | CSS | Bootstrap | JavaScript | jQuery | React | NodeJS | Express </p>
+                            <p>  MongoDB | SQL Server | REST API | Git | Linux | Java </p>
+                            
+                            <div className="social-links">
+
+                                {/* LinkedIn */}
+                                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+
+                                {/* Github */}
+                                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+
+                                {/* Freecodecamp */}
+                                <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-free-code-camp" aria-hidden="true"/>
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
